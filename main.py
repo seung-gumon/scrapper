e0c6eb06a325eb73611287e909f23d644d02cef1
seung-gumon <64651532+seung-gumon@users.noreply.github.com>
2023-08-28 01:35:16 +0900

    test 중

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ dc_inside_url = "https://gall.dcinside.com";
 today_humor_url = "http://www.todayhumor.co.kr";
 ruri_url = "https://bbs.ruliweb.com/";
 fm_korea = "www.fmkorea.com";
-nate_pann = "pann.nate.com";
+nate_pann = "https://pann.nate.com";
 bobaeddream = "https://www.bobaedream.co.kr";
 instiz = "https://www.instiz.net/"
 
@@ -23,7 +23,9 @@ class LinkExtractor:
 
     def extract_links(self,community_url , limit):
         soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        print("community_url ::: ",community_url);
         links = soup.find_all('a', href=lambda x: x and x.startswith(community_url), limit=limit)
+        print("Links :::" , links);
         for link in links:
             self.collected_links.append(link.get('href'))
 
@@ -32,12 +34,12 @@ class LinkExtractor:
 
     def run(self,community_url, limit = 10):
         self.load_page("https://todaybeststory.com/community.html")
-        self.extract_links(community_url)
+        self.extract_links(community_url , limit)
         self.close_driver()
 
 # 객체 생성 및 실행
 extractor = LinkExtractor()
-extractor.run(dc_inside_url)
+extractor.run(nate_pann)
 # 추출한 링크 확인
 print("Collected Links Length:", extractor.collected_links)
 print("Collected Links Length:", len(extractor.collected_links))
