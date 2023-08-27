0123ae66375ff7e41553da185b3df1f242e1562b
seung-gumon <64651532+seung-gumon@users.noreply.github.com>
2023-08-27 23:52:47 +0900

    커뮤니티 글 모으는 스크랩퍼 셀레니움 사용해서 만드는중

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-from requests import get
+from selenium import webdriver
 from bs4 import BeautifulSoup
-from extractors.wwr import extract_wwr_jobs
 
-jobs = extract_wwr_jobs("python")
-print(jobs)
+
+dc_inside_url = "https://gall.dcinside.com";
+
+class LinkExtractor:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.collected_links = []
+
+    def load_page(self, url):
+        self.driver.get(url)
+        self.driver.implicitly_wait(5)
+
+    def extract_links(self,community_url , limit):
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        links = soup.find_all('a', href=lambda x: x and x.startswith(community_url), limit=limit)
+        for link in links:
+            self.collected_links.append(link.get('href'))
+
+    def close_driver(self):
+        self.driver.quit()
+
+    def run(self,community_url, limit = 10):
+        self.load_page("https://todaybeststory.com/community.html")
+        self.extract_links(community_url)
+        self.close_driver()
+
+# 객체 생성 및 실행
+extractor = LinkExtractor()
+extractor.run(dc_inside_url)
+# 추출한 링크 확인
+print("Collected Links Length:", extractor.collected_links)
+print("Collected Links Length:", len(extractor.collected_links))
