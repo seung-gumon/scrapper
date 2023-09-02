ffcb69687a22c46e9d68e3e2d7c8ac87f4717d41
seung-gumon <64651532+seung-gumon@users.noreply.github.com>
2023-09-03 03:01:35 +0900

    transform_links_to_jso 추가

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import json
 
+# 웹사이트 URL과 이름 매핑
+site_names = {
+    "https://gall.dcinside.com": "디씨인사이드",
+    "http://www.todayhumor.co.kr": "오늘의 유머",
+    "https://bbs.ruliweb.com": "루리웹",
+    "https://www.fmkorea.com": "FM Korea",
+    "https://pann.nate.com": "네이트 판",
+    "https://www.bobaedream.co.kr": "보배드림",
+    "http://www.etoland.co.kr": "이토랜드",
+}
 
-dc_inside_url = "https://gall.dcinside.com";
-today_humor_url = "http://www.todayhumor.co.kr";
-ruri_url = "https://bbs.ruliweb.com";
-fm_korea = "https://www.fmkorea.com";
-nate_pann = "https://pann.nate.com";
-bobaeddream = "https://www.bobaedream.co.kr";
-etoland = "http://www.etoland.co.kr";
-# instiz = "https://www.instiz.net"
-
+# 나머지 코드는 동일
+dc_inside_url = "https://gall.dcinside.com"
+today_humor_url = "http://www.todayhumor.co.kr"
+ruri_url = "https://bbs.ruliweb.com"
+fm_korea = "https://www.fmkorea.com"
+nate_pann = "https://pann.nate.com"
+bobaeddream = "https://www.bobaedream.co.kr"
+etoland = "http://www.etoland.co.kr"
 
 web_crawling_target = "https://todaybeststory.com/community.html"
 
@@ -23,8 +33,6 @@ comunity_links = [
     nate_pann,
     bobaeddream
 ]
-
-
 
 class LinkExtractor:
     def __init__(self):
@@ -39,22 +47,33 @@ class LinkExtractor:
         soup = BeautifulSoup(self.driver.page_source, 'html.parser')
         for comunity_link in comunity_links:
             links = soup.find_all('a', href=lambda x: x and x.startswith(comunity_link), limit=limit)
-            self.collected_links[comunity_link] = []  # 이 커뮤니티에 대한 빈 리스트 초기화
+            self.collected_links[comunity_link] = []  
             for link in links:
                 href = link.get('href')
-                self.collected_links[comunity_link].append(href)  # 이 커뮤니티에 대한 리스트에 추가
-
-
+                self.collected_links[comunity_link].append(href)  
+    
     def close_driver(self):
         self.driver.quit()
 
-    def run(self,limit = 10):
+    def transform_links_to_json(self):
+        transformed_data = []
+        for site_url, links in self.collected_links.items():
+            site_name = site_names.get(site_url, "Unknown site")
+            transformed_data.append({
+                "site_url": site_url,
+                "site_name": site_name,
+                "links": links
+            })
+        return json.dumps(transformed_data, indent=2, ensure_ascii=False)
+
+    def run(self, limit=10):
         self.load_page(web_crawling_target)
         self.extract_links(limit)
         self.close_driver()
+        return self.transform_links_to_json()
 
 # 객체 생성 및 실행
 extractor = LinkExtractor()
-extractor.run()
-# 추출한 링크 확인
-print("Collected Links Length:", extractor.collected_links)
+transformed_json = extractor.run()
+print("Transformed JSON Data:")
+print(transformed_json)
