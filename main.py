cc194e51d3b19e546c0bc374cc610345886240f7
seung-gumon <64651532+seung-gumon@users.noreply.github.com>
2023-09-03 02:58:03 +0900

    data 다 가져오게끔 수정

--- main.py
+++ main.py
@@ -4,44 +4,57 @@ from bs4 import BeautifulSoup
 
 dc_inside_url = "https://gall.dcinside.com";
 today_humor_url = "http://www.todayhumor.co.kr";
-ruri_url = "https://bbs.ruliweb.com/";
-fm_korea = "www.fmkorea.com";
+ruri_url = "https://bbs.ruliweb.com";
+fm_korea = "https://www.fmkorea.com";
 nate_pann = "https://pann.nate.com";
 bobaeddream = "https://www.bobaedream.co.kr";
-instiz = "https://www.instiz.net/"
+etoland = "http://www.etoland.co.kr";
+# instiz = "https://www.instiz.net"
 
-comunity_links= [dc_inside_url, today_humor_url, ruri_url, fm_korea, nate_pann, bobaeddream, instiz]
+
+web_crawling_target = "https://todaybeststory.com/community.html"
+
+comunity_links = [
+    etoland,
+    dc_inside_url,
+    today_humor_url, 
+    ruri_url, 
+    fm_korea,
+    nate_pann,
+    bobaeddream
+]
 
 
 
 class LinkExtractor:
     def __init__(self):
         self.driver = webdriver.Chrome()
-        self.collected_links = []
+        self.collected_links = {}
 
     def load_page(self, url):
         self.driver.get(url)
         self.driver.implicitly_wait(5)
 
-    def extract_links(self,community_url , limit):
+    def extract_links(self, limit):
         soup = BeautifulSoup(self.driver.page_source, 'html.parser')
         for comunity_link in comunity_links:
-            print("Extracting Links from", comunity_link)
-            links = soup.find_all('a', href=lambda x: x and x.startswith(community_url), limit=limit)
+            links = soup.find_all('a', href=lambda x: x and x.startswith(comunity_link), limit=limit)
+            self.collected_links[comunity_link] = []  # 이 커뮤니티에 대한 빈 리스트 초기화
             for link in links:
-                href = link.get('href');
-                self.collected_links[comunity_link] = href
+                href = link.get('href')
+                self.collected_links[comunity_link].append(href)  # 이 커뮤니티에 대한 리스트에 추가
+
 
     def close_driver(self):
         self.driver.quit()
 
-    def run(self,community_url, limit = 10):
-        self.load_page("https://todaybeststory.com/community.html")
-        self.extract_links(community_url , limit)
+    def run(self,limit = 10):
+        self.load_page(web_crawling_target)
+        self.extract_links(limit)
         self.close_driver()
 
 # 객체 생성 및 실행
 extractor = LinkExtractor()
-extractor.run(nate_pann)
+extractor.run()
 # 추출한 링크 확인
-print("Collected Links Length:", len(extractor.collected_links))
+print("Collected Links Length:", extractor.collected_links)
