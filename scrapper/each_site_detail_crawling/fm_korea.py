6f6315991a2f77b64326f748c030e7014555ab37
seung-gumon <64651532+seung-gumon@users.noreply.github.com>
2023-09-16 16:45:31 +0900

    make fm_korea class

--- scrapper/each_site_detail_crawling/fm_korea.py
+++ scrapper/each_site_detail_crawling/fm_korea.py
@@ -1,16 +1,17 @@
-def fm_korea(soup):
-    try:
-      return_dict = {}
-      post_html = soup.find("div", {"id": "bd_capture"})
-      title = post_html.find("h1").text
-      if not title:
-            raise Exception('empty title')
-      return {
-            'post_title' : title
-      }
-    except Exception as e:
-        if str(e) == 'empty title':
-            return 'empty title'
-        return {
-          
-        }
+from datetime import datetime
+from bs4 import BeautifulSoup
+from scrapper.upload_images import upload_images
+from scrapper.base_scrapper import BaseScrapper
+
+class FmKoreaScrapper(BaseScrapper):
+  def __init__(self):
+    super().__init__('fm_korea','https://www.fmkorea.com')
+    
+  def get_content_area(self, soup):
+    return soup.find('div' , {'class' : "xe_content"})
+  
+  def get_title(self , soup):
+    return soup.find("span" , {"class" : "np_18px_span"}).text
+  
+  def get_created_at(self , soup):
+    return soup.find("span" , {"class" : "date m_no"}).text
